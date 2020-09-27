2b3d1d06fa413049b5207bd24c895b6fc06c57be
Yerbolat <twinspazyl@gmail.com>
2020-09-27 23:57:19 +0600

    Assignment 3 Commit - 1.2

--- src/app/home/editorial-box/editorial-box.component.ts
+++ src/app/home/editorial-box/editorial-box.component.ts
@@ -8,10 +8,10 @@ import {Movie} from '../../objects/movie';
 })
 export class EditorialBoxComponent implements OnInit {
   movies: Movie[] = [
-    new Movie(3000, 6170, '1 h 42 min', '../../assets/images/movies/movie1.webp', 'Fantastic Beasts and Where to Find Them', 9.3),
-    new Movie(3000, 6170, '1 h 42 min', '../../assets/images/movies/movie2.webp', 'A Dog\'s Purpose 2', 5.1),
-    new Movie(3000, 6170, '1 h 42 min', '../../assets/images/movies/movie3.webp', 'A Dog\'s Purpose 2', 3.5),
-    new Movie(3000, 6170, '1 h 42 min', '../../assets/images/movies/movie4.webp', 'A Dog\'s Purpose 2', 2.2),
+    new Movie(3000, 6170, '1 h 42 min', '../../assets/images/movies/mmovie1.jpg', 'Fantastic Beasts and Where to Find Them', 9.3),
+    new Movie(3000, 6170, '1 h 42 min', '../../assets/images/movies/mmovie2.webp', 'A Dog\'s Purpose 2', 5.1),
+    new Movie(3000, 6170, '1 h 42 min', '../../assets/images/movies/mmovie3.webp', 'A Dog\'s Purpose 2', 3.5),
+    new Movie(3000, 6170, '1 h 42 min', '../../assets/images/movies/mmovie4.jpg', 'A Dog\'s Purpose 2', 2.2),
   ];
 
   constructor() { }
