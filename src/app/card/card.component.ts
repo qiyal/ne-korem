edb023c9130ab8187b57f1060296b9d5fe264ca5
Yerbolat <twinspazyl@gmail.com>
2020-09-30 10:57:30 +0600

    add Live Cycle Hooks

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter, OnChanges, OnDestroy, SimpleChanges} from '@angular/core';
 import {Movie} from '../objects/movie';
 
 @Component({
@@ -6,15 +6,36 @@ import {Movie} from '../objects/movie';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent implements OnInit {
+export class CardComponent implements OnChanges, OnInit, OnDestroy {
+  @Output() deleteMoviesInHistoryWatching = new EventEmitter<number>();
   @Input() movie: Movie;
+  @Input() movieIndex: number;
+
   percent: string;
 
   constructor() { }
 
+  ngOnChanges(changes: SimpleChanges) {
+    for (const propName in changes) {
+      const chng = changes[propName];
+      const cur  = JSON.stringify(chng.currentValue);
+      const prev = JSON.stringify(chng.previousValue);
+      console.log(`CardComponent: ${propName}: currentValue = ${cur}, previousValue = ${prev}`);
+    }
+  }
+
   ngOnInit(): void {
     this.percent = Math.floor(this.movie.time * 100 / this.movie.totalTime) + '%';
-    console.log(this.percent);
+    console.log('CardComponent: OnInit(set percent variable)');
+  }
+
+  ngOnDestroy() {
+    console.log('CardComponent: OnDestroy');
+  }
+
+  deleteMovieInHistory() {
+    // console.log("index dlete: " + this.movieIndex);
+    this.deleteMoviesInHistoryWatching.emit(this.movieIndex);
   }
 
 }
--- src/app/home/continue-viewing/continue-viewing.component.ts
+++ src/app/home/continue-viewing/continue-viewing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
 import {Movie} from '../../objects/movie';
 
 @Component({
@@ -6,7 +6,7 @@ import {Movie} from '../../objects/movie';
   templateUrl: './continue-viewing.component.html',
   styleUrls: ['./continue-viewing.component.scss']
 })
-export class ContinueViewingComponent implements OnInit {
+export class ContinueViewingComponent implements OnChanges, OnInit, OnDestroy {
   movies: Movie[] = [
     new Movie(3000, 6170, '1 h 42 min', '../../assets/images/movies/movie7.webp', 'Fantastic Beasts and Where to Find Them', 9.0),
     new Movie(1400, 6170, '1 h 21 min', '../../assets/images/movies/movie9.webp', 'A Dog\'s Purpose 2', 5.0),
@@ -14,7 +14,28 @@ export class ContinueViewingComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges) {
+    for (const propName in changes) {
+      const chng = changes[propName];
+      const cur  = JSON.stringify(chng.currentValue);
+      const prev = JSON.stringify(chng.previousValue);
+      console.log(`ContinueViewingComponent: ${propName}: currentValue = ${cur}, previousValue = ${prev}`);
+    }
   }
+
+  ngOnInit(): void {
+    console.log('ContinueViewingComponent: OnInit');
+  }
+
+  ngOnDestroy() {
+    console.log('ContinueViewingComponent: OnDestroy');
+  }
+
+  deleteInArr(index: number) {
+    console.log(index);
+    this.movies.splice(index, 1);
+    // console.log(this.movies);
+  }
+
 
 }
